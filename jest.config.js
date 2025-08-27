27e3711ae5b1add235583c2bb3f7561c1597cf38
kaique64 <kaiquehenrique.2005@outlook.com>
2025-08-26 21:19:43 -0300

    update dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   moduleFileExtensions: ["js", "ts", "json", "vue"],
   testEnvironment: "jsdom",
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'], 
   transform: {
     "^.+\\.ts$": "ts-jest",
     "^.+\\.vue$": "@vue/vue3-jest",
